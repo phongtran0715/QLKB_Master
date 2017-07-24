1322db8bbe416b8f17593bc48c23980071d764cf
phongtran0715 <phongtran0715@gmail.com>
2017-07-25 00:27:01 +0700

    update sql script create database

--- PhanMemNoiSoi/bin/Debug/CreateDB/CreateDataBaseQuangHuy.sql
+++ PhanMemNoiSoi/bin/Debug/CreateDB/CreateDataBaseQuangHuy.sql
@@ -4,32 +4,33 @@ if not ('QuangHuyMedical' in (select name from sysdatabases))
 go
 
 use QuangHuyMedical
-
+go
 -- (1) UserList
 if not exists (select name from sysobjects where name = 'UserList' and type='U')
 
 CREATE TABLE [dbo].[UserList](
-	[UserNum] [nchar](100) COLLATE Vietnamese_CI_AS NOT NULL,
+	[UserId] [int] IDENTITY(0,1) NOT NULL,
+	--[UserId] [int] NOT NULL,
 	[UserName] [nchar](100) COLLATE Vietnamese_CI_AS NOT NULL,
 	[Sex] [nchar](10) COLLATE Vietnamese_CI_AS NOT NULL,
-	[WorkGroup] [nchar](20) COLLATE Vietnamese_CI_AS NULL,
+	[WorkGroup] [nchar](50) COLLATE Vietnamese_CI_AS NOT NULL,
 	[Password] [nchar](20) COLLATE Vietnamese_CI_AS NULL,
 	[USERTYPE] [nchar](20) COLLATE Vietnamese_CI_AS NULL,
-	[UserId] [int] IDENTITY(1,1) NOT NULL,
- CONSTRAINT [PK__UserList__7F60ED59] PRIMARY KEY CLUSTERED 
+ CONSTRAINT [PK_UserList] PRIMARY KEY CLUSTERED 
 (
-	[UserNum] ASC
+	[UserId] ASC
 )WITH (PAD_INDEX  = OFF, IGNORE_DUP_KEY = OFF) ON [PRIMARY]
 ) ON [PRIMARY]
-
+begin transaction
+INSERT INTO [QuangHuyMedical].[dbo].[UserList]([UserName],[Sex],[WorkGroup],[Password],[USERTYPE]) VALUES ('sys','male','','admin','')
+commit transaction
 
 
 --(2)	SickData
 if not exists (select name from sysobjects where name = 'SickData' and type='U')
-
 CREATE TABLE [dbo].[SickData](
 	[Num] [int] IDENTITY(1,1) NOT NULL,
-	[SickNum] [nchar](20) NOT NULL,
+	[SickNum] [nchar](20) COLLATE Vietnamese_CI_AS NOT NULL,
 	[SickName] [nchar](70) COLLATE Vietnamese_CI_AS NOT NULL,
 	[Sex] [nchar](10) COLLATE Vietnamese_CI_AS NULL,
 	[Birthday] [datetime] NULL,
@@ -60,6 +61,7 @@ CREATE TABLE [dbo].[HospitalInfo](
 	[HospitalPost] [nchar](20) COLLATE Vietnamese_CI_AS NULL
 ) ON [PRIMARY]
 
+
 --(4)	CheckRecord
 if not exists(select name from sysobjects where name = 'CheckRecord' and type='U')
 CREATE TABLE [dbo].[CheckRecord](
@@ -68,6 +70,7 @@ CREATE TABLE [dbo].[CheckRecord](
 	[ItemCode] [nchar](6) COLLATE Vietnamese_CI_AS NULL,
 	[ItemContentCode] [char](6) COLLATE Vietnamese_CI_AS NULL
 ) ON [PRIMARY]
+
 
 
 --(5)	CheckItemContent
@@ -80,6 +83,7 @@ CREATE TABLE [dbo].[CheckItemContent](
 	[ShowNum] [int] NULL
 ) ON [PRIMARY]
 
+
 --(6)	CheckItem
 if not exists (select name from sysobjects where name = 'CheckItem' and type='U')
 CREATE TABLE [dbo].[CheckItem](
@@ -90,6 +94,7 @@ CREATE TABLE [dbo].[CheckItem](
 	[ShowNum] [int] NULL,
 	[IsDisplay] [int] NULL CONSTRAINT [DF_CheckItem_IsDisplay]  DEFAULT ((0))
 ) ON [PRIMARY]
+
 
 --(10)	BackupInfo
 if not exists (select name from sysobjects where name = 'BackupInfo' and type='U')
@@ -103,13 +108,44 @@ CREATE TABLE [dbo].[BackupInfo](
 ) ON [PRIMARY]
 
 
-INSERT INTO [QuangHuyMedical].[dbo].[UserList]
-           ([UserNum]
-           ,[UserName]
-           ,[Sex]
-           ,[WorkGroup]
-           ,[Password]
-           ,[USERTYPE])
-     VALUES
-           ('1','sys','male','','admin','')
+--(11) Roles LIST
+CREATE TABLE [dbo].[RolesList](
+	[Num] [int] NOT NULL,
+	[RoleName] [nchar](50) NOT NULL
+) ON [PRIMARY]
+begin transaction
+INSERT INTO [QuangHuyMedical].[dbo].[RolesList]([Num],[RoleName]) VALUES ('0',N'Tạo tài khoản mới')
+INSERT INTO [QuangHuyMedical].[dbo].[RolesList]([Num],[RoleName]) VALUES ('1',N'Sửa thông tin tài khoản')
+INSERT INTO [QuangHuyMedical].[dbo].[RolesList]([Num],[RoleName]) VALUES ('2',N'Xóa tài khoản')
+INSERT INTO [QuangHuyMedical].[dbo].[RolesList]([Num],[RoleName]) VALUES ('3',N'Thay đổi mật khẩu tài khoản')
+INSERT INTO [QuangHuyMedical].[dbo].[RolesList]([Num],[RoleName]) VALUES ('4',N'Cài đặt hệ thống')
+INSERT INTO [QuangHuyMedical].[dbo].[RolesList]([Num],[RoleName]) VALUES ('5',N'Xem báo cáo')
+INSERT INTO [QuangHuyMedical].[dbo].[RolesList]([Num],[RoleName]) VALUES ('6',N'Xóa bệnh nhân')
+INSERT INTO [QuangHuyMedical].[dbo].[RolesList]([Num],[RoleName]) VALUES ('7',N'Thay đổi danh mục khám bệnh')
+INSERT INTO [QuangHuyMedical].[dbo].[RolesList]([Num],[RoleName]) VALUES ('8',N'Sao lưu , khôi phục dữ liệu ')
+INSERT INTO [QuangHuyMedical].[dbo].[RolesList]([Num],[RoleName]) VALUES ('9',N'Xem lịch sử hệ thống')
+commit transaction
+
+
+--(12) User Group
+CREATE TABLE [dbo].[UserGroup](
+	[Num] [nchar](50) NOT NULL,
+	[GroupNameHeader] [nchar](50) COLLATE Vietnamese_CI_AS NOT NULL,
+	[Id] [int] NULL
+) ON [PRIMARY]
+begin transaction
+INSERT INTO [QuangHuyMedical].[dbo].[UserGroup]([Num],[GroupNameHeader],[Id]) VALUES (N'Admin',N'Admin','2')
+INSERT INTO [QuangHuyMedical].[dbo].[UserGroup]([Num],[GroupNameHeader],[Id]) VALUES (N'Manager',N'Quản Lý','1')
+INSERT INTO [QuangHuyMedical].[dbo].[UserGroup]([Num],[GroupNameHeader],[Id]) VALUES (N'Doctor',N'Bác sỹ','0')
+commit transaction
+
+
+--(13) User Group Role
+CREATE TABLE [dbo].[UserGroupRole](
+	[Num] [int] IDENTITY(1,1) NOT NULL,
+	[GroupId] [nchar](50) NOT NULL,
+	[RoleId] [int] NOT NULL
+) ON [PRIMARY]
+
+
 		   
