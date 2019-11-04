c8282c8e262571b9cd60741daf45d33f9f39f316
phongtran0715 <phongtran0715@gmail.com>
2019-11-04 17:39:18 +0700

    Add note image form

---
+++ PhanMemNoiSoi/Glossary/NewNoteImage.Designer.cs
@@ -0,0 +1,129 @@
+﻿namespace PhanMemNoiSoi
+{
+    partial class NewNoteImage
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(NewGlossaryItem));
+            this.gbParent = new System.Windows.Forms.GroupBox();
+            this.txtNoiDung = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.btnOK = new System.Windows.Forms.Button();
+            this.btnCancle = new System.Windows.Forms.Button();
+            this.gbParent.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // gbParent
+            // 
+            this.gbParent.Controls.Add(this.txtNoiDung);
+            this.gbParent.Controls.Add(this.label3);
+            this.gbParent.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.gbParent.Location = new System.Drawing.Point(5, 5);
+            this.gbParent.Name = "gbParent";
+            this.gbParent.Size = new System.Drawing.Size(438, 153);
+            this.gbParent.TabIndex = 0;
+            this.gbParent.TabStop = false;
+            this.gbParent.Text = "Thông tin";
+            // 
+            // txtNoiDung
+            // 
+            this.txtNoiDung.Location = new System.Drawing.Point(92, 54);
+            this.txtNoiDung.MaxLength = 99;
+            this.txtNoiDung.Multiline = true;
+            this.txtNoiDung.Name = "txtNoiDung";
+            this.txtNoiDung.ScrollBars = System.Windows.Forms.ScrollBars.Both;
+            this.txtNoiDung.Size = new System.Drawing.Size(340, 90);
+            this.txtNoiDung.TabIndex = 4;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(6, 32);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(72, 20);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Nội dung";
+            // 
+            // btnOK
+            // 
+            this.btnOK.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnOK.Image = global::PhanMemNoiSoi.Properties.Resources.checked_24x24;
+            this.btnOK.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.btnOK.Location = new System.Drawing.Point(241, 168);
+            this.btnOK.Name = "btnOK";
+            this.btnOK.Size = new System.Drawing.Size(98, 35);
+            this.btnOK.TabIndex = 1;
+            this.btnOK.Text = "    OK";
+            this.btnOK.UseVisualStyleBackColor = true;
+            this.btnOK.Click += new System.EventHandler(this.btnOK_Click);
+            // 
+            // btnCancle
+            // 
+            this.btnCancle.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnCancle.Image = global::PhanMemNoiSoi.Properties.Resources.delete_24x24;
+            this.btnCancle.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.btnCancle.Location = new System.Drawing.Point(345, 168);
+            this.btnCancle.Name = "btnCancle";
+            this.btnCancle.Size = new System.Drawing.Size(98, 35);
+            this.btnCancle.TabIndex = 2;
+            this.btnCancle.Text = "    Thoát";
+            this.btnCancle.UseVisualStyleBackColor = true;
+            this.btnCancle.Click += new System.EventHandler(this.btnCancle_Click);
+            // 
+            // NewGlossaryItem
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.SeaShell;
+            this.ClientSize = new System.Drawing.Size(447, 212);
+            this.Controls.Add(this.btnCancle);
+            this.Controls.Add(this.btnOK);
+            this.Controls.Add(this.gbParent);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "NewGlossaryItem";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Thêm mới danh mục";
+            this.Load += new System.EventHandler(this.EditGlossary_Load);
+            this.gbParent.ResumeLayout(false);
+            this.gbParent.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.GroupBox gbParent;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Button btnOK;
+        private System.Windows.Forms.Button btnCancle;
+        private System.Windows.Forms.TextBox txtNoiDung;
+    }
+}
---
+++ PhanMemNoiSoi/Glossary/NewNoteImage.cs
@@ -0,0 +1,47 @@
+﻿using OD.Forms.Security;
+using System;
+using System.Security.Principal;
+using System.Windows.Forms;
+
+namespace PhanMemNoiSoi
+{
+    //public partial class NewNoteIamge : SecureBaseForm
+    public partial class NewNoteImage : Form
+    {
+        //Declare delegate for transfer data
+        public delegate void UpdateDataHandler(string msg);
+        public UpdateDataHandler updateData;
+
+        public NewNoteImage(IPrincipal userPrincipal) 
+            //: base(RolesList.Instance.roleList.ToArray(), userPrincipal)
+        {
+            InitializeComponent();
+        }
+
+        private void btnCancle_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private void EditGlossary_Load(object sender, EventArgs e)
+        {
+        }
+
+        private void btnOK_Click(object sender, EventArgs e)
+        {
+            string content = txtNoiDung.Text.ToString().Trim();
+            if (content != "")
+            {
+                if (updateData != null)
+                {
+                    updateData(txtNoiDung.Text.ToString().Trim());
+                }
+                this.Close();
+            }
+            else
+            {
+                MessageBox.Show("Nội dung không được để trống!", "Thông báo", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+    }
+}
